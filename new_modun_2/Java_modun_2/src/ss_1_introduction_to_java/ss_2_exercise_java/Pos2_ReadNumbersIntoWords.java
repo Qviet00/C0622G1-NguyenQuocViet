57f67e3ee4c294cc22893d7f8e8af0ed6d60864c
Qviet00 <108318259+Qviet00@users.noreply.github.com>
2022-08-01 16:27:17 +0700

    up

--- new_modun_2/Java_modun_2/src/ss_1_introduction_to_java/ss_2_exercise_java/Pos2_ReadNumbersIntoWords.java
+++ new_modun_2/Java_modun_2/src/ss_1_introduction_to_java/ss_2_exercise_java/Pos2_ReadNumbersIntoWords.java
@@ -7,237 +7,115 @@ public class Pos2_ReadNumbersIntoWords {
         Scanner src = new Scanner(System.in);
         System.out.print("Nhập số đi bạn:");
         int number = src.nextInt();
-        String integer = null;
-        String surplus = null;
-        String surplus1 = null;
-        if (number >= 0 && number <= 20) {
-            switch (number) {
+        String hundreds = null;
+        String dozens = null;
+        String unit = null;
+
+        if (number < 1001 && number >= 0) {
+            int hundreds1 = number / 100;
+            int dozens1 = number % 100;
+            int interger3 = dozens1 / 10;
+            int unit1 = dozens1 % 10;
+            switch (hundreds1) {
                 case 0:
-                    System.out.println("Zero");
+                    hundreds = "";
                     break;
                 case 1:
-                    System.out.println("One");
+                    hundreds = "Onehundred";
                     break;
                 case 2:
-                    System.out.println("Two");
+                    hundreds = "Twohundred";
                     break;
                 case 3:
-                    System.out.println("There");
+                    hundreds = "Therehundred";
                     break;
                 case 4:
-                    System.out.println("Fourt");
+                    hundreds = "Fourthundred";
                     break;
                 case 5:
-                    System.out.println("Five");
+                    hundreds = "Fivehundred";
                     break;
                 case 6:
-                    System.out.println("Six");
+                    hundreds = "Sixhundred";
                     break;
                 case 7:
-                    System.out.println("Sevent");
+                    hundreds = "Seventhundred";
                     break;
                 case 8:
-                    System.out.println("Eight");
+                    hundreds = "Eighthundred ";
                     break;
                 case 9:
-                    System.out.println("Nice");
-                    break;
-                case 10:
-                    System.out.println("Ten");
-                    break;
-                case 11:
-                    System.out.println("Eleven");
-                    break;
-                case 12:
-                    System.out.println("Twelve");
-                    break;
-                case 13:
-                    System.out.println("Thirteen");
-                    break;
-                case 14:
-                    System.out.println("Fourteen");
-                    break;
-                case 15:
-                    System.out.println("Fifteen");
-                    break;
-                case 16:
-                    System.out.println("Sixteen");
-                    break;
-                case 17:
-                    System.out.println("Seventeen");
-                    break;
-                case 18:
-                    System.out.println("Eighteen");
-                    break;
-                case 19:
-                    System.out.println("Nineteen");
-                    break;
-                case 20:
-                    System.out.println("Twenty");
+                    hundreds = "Ninehundred";
                     break;
             }
+            switch (interger3) {
+                case 0:
+                    dozens = "";
+                    break;
+                case 1:
+                    dozens = "and Ten";
+                    break;
+                case 2:
+                    dozens = "and Twenty";
+                    break;
+                case 3:
+                    dozens = "and Thirty";
+                    break;
+                case 4:
+                    dozens = "and Fourty";
+                    break;
+                case 5:
+                    dozens = "and Fitty";
+                    break;
+                case 6:
+                    dozens = "and Sixty";
+                    break;
+                case 7:
+                    dozens = "and Sevennty";
+                    break;
+                case 8:
+                    dozens = "and Eightty";
+                    break;
+                case 9:
+                    dozens = "and Ninety";
+                    break;
+            }
+            switch (unit1) {
+                case 0:
+                    unit = "";
+                    break;
+                case 1:
+                    unit = "One";
+                    break;
+                case 2:
+                    unit = "Two";
+                    break;
+                case 3:
+                    unit = "Three";
+                    break;
+                case 4:
+                    unit = "Fourt";
+                    break;
+                case 5:
+                    unit = "Five";
+                    break;
+                case 6:
+                    unit = "Six";
+                    break;
+                case 7:
+                    unit = "Sevent";
+                    break;
+                case 8:
+                    unit = "Eight";
+                    break;
+                case 9:
+                    unit = "Nine";
+                    break;
+            }
+            System.out.println(hundreds + " " + dozens + " " + unit);
         } else {
-            if (number < 100) {
-                switch (number / 10) {
-                    case 2:
-                        integer = "Twenty";
-                        break;
-                    case 3:
-                        integer = "Thirty";
-                        break;
-                    case 4:
-                        integer = "Fourty";
-                        break;
-                    case 5:
-                        integer = "Fitty";
-                        break;
-                    case 6:
-                        integer = "Sixty";
-                        break;
-                    case 7:
-                        integer = "Sevennty";
-                        break;
-                    case 8:
-                        integer = "Eightty";
-                        break;
-                    case 9:
-                        integer = "Nicety";
-                        break;
-                }
-                switch (number % 10) {
-                    case 1:
-                        surplus = "One";
-                        break;
-                    case 2:
-                        surplus = "Two";
-                        break;
-                    case 3:
-                        surplus = "There";
-                        break;
-                    case 4:
-                        surplus = "Fourt";
-                        break;
-                    case 5:
-                        surplus = "Five";
-                        break;
-                    case 6:
-                        surplus = "Six";
-                        break;
-                    case 7:
-                        surplus = "Sevent";
-                        break;
-                    case 8:
-                        surplus = "Eight";
-                        break;
-                    case 9:
-                        surplus = "Nice";
-                        break;
-                }
-                System.out.println(integer + " " + surplus);
-            } else {
-                if (number < 1001) {
-                    int interger1 = number / 100;
-                    int surplus2 = number % 100;
-                    int interger3 = surplus2 / 10;
-                    int interger4 = surplus2 % 10;
-                    switch (interger1) {
-                        case 1:
-                            integer = "One hundred";
-                            break;
-                        case 2:
-                            integer = "Two hundred";
-                            break;
-                        case 3:
-                            integer = "There hundred";
-                            break;
-                        case 4:
-                            integer = "Fourt hundred";
-                            break;
-                        case 5:
-                            integer = "Five hundred";
-                            break;
-                        case 6:
-                            integer = "Six hundred";
-                            break;
-                        case 7:
-                            integer = "Sevent hundred";
-                            break;
-                        case 8:
-                            integer = "Eight hundred ";
-                            break;
-                        case 9:
-                            integer = "Nice hundred";
-                            break;
-                    }
-                    switch (interger3) {
-                        case 0:
-                            surplus = "";
-                            break;
-                        case 1:
-                            surplus = "and Ten";
-                            break;
-                        case 2:
-                            surplus = "and Twenty";
-                            break;
-                        case 3:
-                            surplus = "and Thirty";
-                            break;
-                        case 4:
-                            surplus = "and Fourty";
-                            break;
-                        case 5:
-                            surplus = "and Fitty";
-                            break;
-                        case 6:
-                            surplus = "and Sixty";
-                            break;
-                        case 7:
-                            surplus = "and Sevennty";
-                            break;
-                        case 8:
-                            surplus = "and Eightty";
-                            break;
-                        case 9:
-                            surplus = "and Nicety";
-                            break;
-                    }
-                    switch (interger4) {
-                        case 0:
-                            surplus1 = "";
-                            break;
-                        case 1:
-                            surplus1 = "One";
-                            break;
-                        case 2:
-                            surplus1 = "Two";
-                            break;
-                        case 3:
-                            surplus1 = "Three";
-                            break;
-                        case 4:
-                            surplus1 = "Fourt";
-                            break;
-                        case 5:
-                            surplus1 = "Five";
-                            break;
-                        case 6:
-                            surplus1 = "Six";
-                            break;
-                        case 7:
-                            surplus1 = "Sevent";
-                            break;
-                        case 8:
-                            surplus1 = "Eight";
-                            break;
-                        case 9:
-                            surplus1 = "Nice";
-                            break;
-                    }
-                    System.out.println(integer + " " + surplus + " " + surplus1);
-                }else {
-                    System.out.println(" Out Of Ability");
-                }
-            }
+            System.out.println(" Out Of Ability");
         }
     }
 }
+
