3def8fc19347acbb2498f807ea7a03d64c28e0ab
Qviet00 <108318259+Qviet00@users.noreply.github.com>
2022-08-01 17:11:59 +0700

    up

--- new_modun_2/Java_modun_2/src/ss_1_introduction_to_java/ss_2_exercise_java/Pos2_ReadNumbersIntoWords.java
+++ new_modun_2/Java_modun_2/src/ss_1_introduction_to_java/ss_2_exercise_java/Pos2_ReadNumbersIntoWords.java
@@ -10,111 +10,139 @@ public class Pos2_ReadNumbersIntoWords {
         String hundreds = null;
         String dozens = null;
         String unit = null;
-
-        if (number < 1001 && number >= 0) {
-            int hundreds1 = number / 100;
-            int dozens1 = number % 100;
-            int interger3 = dozens1 / 10;
-            int unit1 = dozens1 % 10;
-            switch (hundreds1) {
-                case 0:
-                    hundreds = "";
+        if (number > 10 && number < 20) {
+            switch (number) {
+                case 11:
+                    System.out.println("Eleven");
                     break;
-                case 1:
-                    hundreds = "Onehundred";
+                case 12:
+                    System.out.println("Twelve");
                     break;
-                case 2:
-                    hundreds = "Twohundred";
+                case 13:
+                    System.out.println("Thirteen");
                     break;
-                case 3:
-                    hundreds = "Therehundred";
+                case 14:
+                    System.out.println("fourteen");
                     break;
-                case 4:
-                    hundreds = "Fourthundred";
+                case 15:
+                    System.out.println("fifteen");
                     break;
-                case 5:
-                    hundreds = "Fivehundred";
+                case 16:
+                    System.out.println("sixteen");
                     break;
-                case 6:
-                    hundreds = "Sixhundred";
+                case 17:
+                    System.out.println("seventeen");
                     break;
-                case 7:
-                    hundreds = "Seventhundred";
+                case 18:
+                    System.out.println("eighteen");
                     break;
-                case 8:
-                    hundreds = "Eighthundred ";
-                    break;
-                case 9:
-                    hundreds = "Ninehundred";
+                case 19:
+                    System.out.println("nineteen");
                     break;
             }
-            switch (interger3) {
-                case 0:
-                    dozens = "";
-                    break;
-                case 1:
-                    dozens = "and Ten";
-                    break;
-                case 2:
-                    dozens = "and Twenty";
-                    break;
-                case 3:
-                    dozens = "and Thirty";
-                    break;
-                case 4:
-                    dozens = "and Fourty";
-                    break;
-                case 5:
-                    dozens = "and Fitty";
-                    break;
-                case 6:
-                    dozens = "and Sixty";
-                    break;
-                case 7:
-                    dozens = "and Sevennty";
-                    break;
-                case 8:
-                    dozens = "and Eightty";
-                    break;
-                case 9:
-                    dozens = "and Ninety";
-                    break;
-            }
-            switch (unit1) {
-                case 0:
-                    unit = "";
-                    break;
-                case 1:
-                    unit = "One";
-                    break;
-                case 2:
-                    unit = "Two";
-                    break;
-                case 3:
-                    unit = "Three";
-                    break;
-                case 4:
-                    unit = "Fourt";
-                    break;
-                case 5:
-                    unit = "Five";
-                    break;
-                case 6:
-                    unit = "Six";
-                    break;
-                case 7:
-                    unit = "Sevent";
-                    break;
-                case 8:
-                    unit = "Eight";
-                    break;
-                case 9:
-                    unit = "Nine";
-                    break;
-            }
-            System.out.println(hundreds + " " + dozens + " " + unit);
         } else {
-            System.out.println(" Out Of Ability");
+            if (number < 1001 && number > 20) {
+                int hundreds1 = number / 100;
+                int dozens1 = number % 100;
+                int interger3 = dozens1 / 10;
+                int unit1 = dozens1 % 10;
+                switch (hundreds1) {
+                    case 0:
+                        hundreds = "";
+                        break;
+                    case 1:
+                        hundreds = "Onehundred";
+                        break;
+                    case 2:
+                        hundreds = "Twohundred";
+                        break;
+                    case 3:
+                        hundreds = "Therehundred";
+                        break;
+                    case 4:
+                        hundreds = "Fourthundred";
+                        break;
+                    case 5:
+                        hundreds = "Fivehundred";
+                        break;
+                    case 6:
+                        hundreds = "Sixhundred";
+                        break;
+                    case 7:
+                        hundreds = "Seventhundred";
+                        break;
+                    case 8:
+                        hundreds = "Eighthundred ";
+                        break;
+                    case 9:
+                        hundreds = "Ninehundred";
+                        break;
+                }
+                switch (interger3) {
+                    case 0:
+                        dozens = "";
+                        break;
+                    case 1:
+                        dozens = "and Ten";
+                        break;
+                    case 2:
+                        dozens = "and Twenty";
+                        break;
+                    case 3:
+                        dozens = "and Thirty";
+                        break;
+                    case 4:
+                        dozens = "and Forty";
+                        break;
+                    case 5:
+                        dozens = "and Fifty";
+                        break;
+                    case 6:
+                        dozens = "and Sixty";
+                        break;
+                    case 7:
+                        dozens = "and Seventy";
+                        break;
+                    case 8:
+                        dozens = "and Eighty";
+                        break;
+                    case 9:
+                        dozens = "and Ninety";
+                        break;
+                }
+                switch (unit1) {
+                    case 1:
+                        unit = "One";
+                        break;
+                    case 2:
+                        unit = "Two";
+                        break;
+                    case 3:
+                        unit = "Three";
+                        break;
+                    case 4:
+                        unit = "Four";
+                        break;
+                    case 5:
+                        unit = "Five";
+                        break;
+                    case 6:
+                        unit = "Six";
+                        break;
+                    case 7:
+                        unit = "Seven";
+                        break;
+                    case 8:
+                        unit = "Eight";
+                        break;
+                    case 9:
+                        unit = "Nine";
+                        break;
+                }
+                    System.out.println(hundreds + " " + dozens + " " + unit);
+            } else {
+                System.out.println(" Out Of Ability");
+            }
         }
     }
 }
