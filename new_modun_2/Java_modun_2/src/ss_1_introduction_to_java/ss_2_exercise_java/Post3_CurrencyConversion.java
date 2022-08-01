88ecaf26905cf8c335507350c9eb43a28fd5bc8d
Qviet00 <108318259+Qviet00@users.noreply.github.com>
2022-08-01 13:38:08 +0700

    up

---
+++ new_modun_2/Java_modun_2/src/ss_1_introduction_to_java/ss_2_exercise_java/Post3_CurrencyConversion.java
@@ -0,0 +1,12 @@
+package ss_1_introduction_to_java.ss_2_exercise_java;
+
+import java.util.Scanner;
+
+public class Post3_CurrencyConversion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số tiền USD:");
+        double vnd = scanner.nextDouble();
+        System.out.println("Số tiền quy đổi:" + vnd * 23000+ "VND");
+    }
+}
