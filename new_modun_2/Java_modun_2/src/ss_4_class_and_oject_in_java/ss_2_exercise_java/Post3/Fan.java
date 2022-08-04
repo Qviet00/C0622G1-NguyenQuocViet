36baba6fcc94881fc70cf37346038972b5b7da95
Qviet00 <108318259+Qviet00@users.noreply.github.com>
2022-08-04 18:34:02 +0700

    up

---
+++ new_modun_2/Java_modun_2/src/ss_4_class_and_oject_in_java/ss_2_exercise_java/Post3/Fan.java
@@ -0,0 +1,66 @@
+package ss_4_class_and_oject_in_java.ss_2_exercise_java.Post3;
+
+public class Fan {
+    final int slow = 1;
+    final int medium = 2;
+
+    final int hard = 3;
+    private int speed;
+    private boolean on = false;
+    private double radius = 5;
+    private String color = "blue";
+
+    public Fan() {
+
+    }
+
+    public Fan(int speed, boolean on, double radius, String color) {
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    public boolean getOn() {
+        return this.on;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        if (this.on){
+            return String.format("Speed: %d Color: %s Radius: %f fan is on",this.speed, this.color, this.radius );
+        }
+        else {
+            return String.format("Speed: %d Color: %s Radius: %f fan is off",this.speed, this.color, this.radius );
+
+        }
+    }
+}
---
+++ new_modun_2/Java_modun_2/src/ss_4_class_and_oject_in_java/ss_2_exercise_java/Post3/Main.java
@@ -0,0 +1,10 @@
+package ss_4_class_and_oject_in_java.ss_2_exercise_java.Post3;
+
+public class Main {
+    public static void main(String[] args) {
+        Fan fanOne = new Fan(5, true, 5, "Yellow");
+        Fan fanTwo = new Fan(5, false, 5, "Yellow");
+        System.out.println(fanOne.toString());
+        System.out.println(fanTwo.toString());
+    }
+}
