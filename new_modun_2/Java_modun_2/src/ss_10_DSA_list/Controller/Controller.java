1dc61a91172c47203c7b0288c8c643db54acbf96
Qviet00 <108318259+Qviet00@users.noreply.github.com>
2022-08-11 21:13:00 +0700

    up

---
+++ new_modun_2/Java_modun_2/src/ss_10_DSA_list/Controller/Controller.java
@@ -0,0 +1,44 @@
+package ss_10_DSA_list.Controller;
+
+import ss_10_DSA_list.Model.MyArrayList;
+
+public class Controller {
+    public static class Student {
+        private final int id;
+        private String name;
+
+        public Student(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student(1, "Nam");
+        Student student2 = new Student(2, "Uyên");
+        Student student3 = new Student(3, "Hãi");
+        Student student4 = new Student(4, "Duy");
+        Student student5 = new Student(5, "Sáng");
+
+        MyArrayList<Student> studentMyArrayList = new MyArrayList<>();
+        studentMyArrayList.add(student1);
+        studentMyArrayList.add(student2);
+        studentMyArrayList.add(student3);
+        studentMyArrayList.add(student4);
+        studentMyArrayList.add(student5);
+        for (int i=0;i<studentMyArrayList.size();i++){
+            Student student= (Student) studentMyArrayList.elements[i];
+            System.out.println(student.getId());
+            System.out.println(student.getName());
+        }
+    }
+}
