6169691e9945e025a13f7cd12407b8d3923b9b2d
unknown <qkietqn@gmail.com>
2022-11-23 21:36:13 +0700

    up

--- new_modun_5/case_study_component/case_study/CaseStudy/src/app/app.module.ts
+++ new_modun_5/case_study_component/case_study/CaseStudy/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { CustomerComponent } from './cusstomer/list/customer.component';
+import { HomeComponent } from './home/home/home.component';
+import { AddCustomerComponent } from './cusstomer/add-customer/add-customer.component';
+import { ListFacilityComponent } from './facility/list-facility/list-facility.component';
+import { EditFacilityComponent } from './facility/edit-facility/edit-facility.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    HomeComponent,
+    AddCustomerComponent,
+    ListFacilityComponent,
+    EditFacilityComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
